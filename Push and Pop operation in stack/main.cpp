53b0925fd0042df4a6c154005dcc825c005a075d
Jeet Mukherjee <91522784+Jeetmu@users.noreply.github.com>
2022-09-13 10:51:13 +0530

    Add files via upload

---
+++ Push and Pop operation in stack/main.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }else{
+        return 0;
+    }
+}
+int isFull(struct stack *ptr)
+{
+    if(ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }else{
+        return 0;
+    }
+}
+void push(struct stack *ptr, int val)
+{
+    if (isFull(ptr))
+    {
+        cout << "Stcak Overflow, can't push any more to the stack";
+    }else{
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
+    }
+}
+
+void pop (struct stack *ptr)
+{
+ if (isEmpty(ptr))
+ {
+    cout << "The stcak is empty";
+ }else{
+    cout << "The element pop out "<< ptr->arr[ptr->top]<<endl;
+    ptr->top--;
+    // int val = ptr->arr[ptr->top];
+    // ptr->top--;
+    // return val;
+ }
+ 
+}
+
+int main()
+{
+    struct stack *p; 
+    //alocate memory at heap
+    p = (struct stack *)malloc(sizeof(struct stack));
+    p->size = 10;
+    p->top = -1;
+    p->arr = (int *)malloc(p->size * sizeof(int));
+    cout << "Stack has been created successfully"<<endl;
+    cout <<"Before pushing, Full: "<<isFull(p) << endl;
+    cout << "Before pushing, Empty: "<< isEmpty(p) <<endl;
+
+    push(p, 35);
+    push(p, 45);
+    push(p, 55);
+    push(p, 65);
+    push(p, 75);
+    push(p, 85);
+    push(p, 95);
+    push(p, 15);
+    push(p, 25);
+    push(p, 05);
+
+    cout <<"Before pushing, Full: "<<isFull(p) << endl;
+    cout << "Before pushing, Empty: "<< isEmpty(p) <<endl;
+
+    pop(p);
+    pop(p);
+    pop(p);
+
+    return 0;
+}
