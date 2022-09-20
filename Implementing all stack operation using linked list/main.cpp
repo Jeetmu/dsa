c9b0de3dbb5889f4be03e1d80efda9300a00fa7a
Jeet Mukherjee <91522784+Jeetmu@users.noreply.github.com>
2022-09-20 10:45:44 +0530

    Add files via upload

---
+++ Implementing all stack operation using linked list/main.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node* top = NULL;
+
+void linkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        cout<< "Element: "<< ptr->data << endl;
+        ptr = ptr->next;
+    }
+}
+int isEmpty(struct Node *top)
+{
+    if(top == NULL){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int isFull(struct Node *ptr){
+    struct Node * p = (struct Node*)malloc(sizeof(struct Node));
+    if (p == NULL){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+struct Node *push (struct Node * top, int x)
+{
+    if(isFull(top)){
+        cout << "Stack Overflow" << endl;
+    }else{
+        struct Node *n = (struct Node *) malloc(sizeof(struct Node));
+        n->data = x;
+        n->next = top;
+        top = n;
+        return top;
+    }
+}
+int pop(struct Node * tp)
+{
+    if(isEmpty(tp)){
+        cout<< "The stack is empty" << endl;
+    }else{
+        struct Node *n = tp;
+        top = (tp)->next;
+        int x = n->data;
+        free(n);
+        return x;
+    }
+}
+
+int main()
+{  
+    top = push(top, 78);
+    top = push(top, 7);
+    top = push(top, 87);
+
+    linkedListTraversal(top);  
+
+    int element = pop(top);
+    cout << "popped element is: " << element <<endl;
+    linkedListTraversal(top);   
+    return 0;
+}
